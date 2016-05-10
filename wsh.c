24f14035e1c270a65d76a58906a1c0d4f762efcd
synthetixa <pinkie11301979@gmail.com>
2016-05-09 20:48:31 -0400

    Added a little more introduction to before the 'Type Something' prompt

--- wsh.c
+++ wsh.c
@@ -2,6 +2,7 @@
 
 int main() {
   char buffer[80];
+  iwrites("Welcome to WhirliOS.\nThis is a 16-bit OS designed by Whirligig231.\n");
   iwrites("Type something: ");
   ireads(buffer);
   iwrites("You typed ");
