27318caadfab2581143f021e8f23c45ef47da3b1
whirligig231 <gigilrihw@gmail.com>
2016-05-10 21:14:43 -0400

    Added read-execute-print loop that currently can't execute anything

--- wsh.c
+++ wsh.c
@@ -2,11 +2,12 @@
 
 int main() {
   char buffer[80];
-  iwrites("Welcome to WhirliOS.\nThis is a 16-bit OS designed by Whirligig231.\n");
-  iwrites("Type something: ");
-  ireads(buffer);
-  iwrites("You typed ");
-  iwrites(buffer);
-  iwrites("!\n");
-  while (1) continue;
+  iwrites("Welcome to WhirliOS.\nThis is a 16-bit OS designed by Whirligig231.\n\n");
+  while (1) {
+    iwrites("/> ");
+    ireads(buffer);
+    iwrites("Error: unrecognized command \"");
+    iwrites(buffer);
+    iwrites("\"\n");
+  }
 }
